86281372d2670649408dd1dfefe50b034fd43f0b
ihsancetinn <ihsancetin92@gmail.com>
2022-11-04 17:54:09 +0300

    login işlemleri

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -8,6 +9,25 @@ exports.createUser = async (req, res) => {
       status: 'success',
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          res.status(200).send('Başarılı Bir Şekilde Giriş Yaptınız');
+        }
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: 'fail',
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,7 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); // /users/signup
+router.route('/login').post(authController.loginUser);
 
 
 
