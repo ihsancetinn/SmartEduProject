2234bc886470e3e66bc3cf2a47eb1d45d2342339
ihsancetinn <ihsancetin92@gmail.com>
2022-11-08 14:34:40 +0300

    Özel (Custom) Middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const authController = require("../controllers/authController");
-
+const authMiddleware = require("../middlewares/authMiddleware")
 
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); // /users/signup
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
-router.route('/dashboard').get(authController.getDasboardPage);
+router.route('/dashboard').get(authMiddleware, authController.getDasboardPage);
 
 
 
