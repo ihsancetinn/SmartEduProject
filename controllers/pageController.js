291c87983e6880dd52e0e724d6c90bc06cb44579
ihsancetinn <ihsancetin92@gmail.com>
2022-11-10 15:36:44 +0300

    Send Mail

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,23 +1,76 @@
 
+const nodemailer = require("nodemailer");
+
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
-    res.status(200).render('index', {
-      page_name: 'index',
-    });
-  };
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+};
 
 exports.getAboutPage = (req, res) => {
   res.status(200).render('about', {
     page_name: 'about',
   });
 };
+
 exports.getRegisterPage = (req, res) => {
   res.status(200).render('register', {
     page_name: 'register',
   });
 };
+
 exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+
+  const outputMessage = `
+  
+  <h1>Mail Details </h1>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+  `
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "ihsancetin92@gmail.com", // gmail account
+      pass: "tmgahkrficzzeurs", // gmail password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart EDU Contact Form" <ihsancetin92@gmail.com>', // sender address
+    to: "ihsancetin50@hotmail.com.tr", // list of receivers
+    subject: "Smart EDU Contact Form New Message ✔", // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect('contact');
+
 };
